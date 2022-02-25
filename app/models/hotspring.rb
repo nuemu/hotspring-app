0798b28a95cd48ac07089155400c10aeeca53ab6
tusko-ku <tusko.ku@gmail.com>
2022-02-25 12:36:01 +0900

    Add:Hotspring model validations

--- app/models/hotspring.rb
+++ app/models/hotspring.rb
@@ -1,2 +1,6 @@
 class Hotspring < ApplicationRecord
+  validates :latitude, presence: true
+  validates :longtitude, presence: true
+
+  enum status: { 'unexplored': 0, 'not_exist': 1, 'ban': 2, 'open': 3 }
 end
