bd8205c7e54267c212953d7cf2d6434cb9516115
tusko-ku <tusko.ku@gmail.com>
2022-02-25 15:38:24 +0900

    Add:view functions Request and show open hotsprings

--- app/javascript/pages/TopPage.vue
+++ app/javascript/pages/TopPage.vue
@@ -1,28 +1,70 @@
 <template>
-  <Map />
+  <div id="map"></div>
 </template>
 
 <script>
-import Map from '../components/Map.vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default{
-  components:{
-    Map
-  },
   data(){
     return{
       hotsprings:[],
+      map:'',
     }
   },
+  created(){
+    L.Icon.Default.imagePath = '.'
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    })
+  },
   mounted(){
-    this.fetchHotsprings()
+    this.map = this.CreateMap();
+    this.fetchHotsprings();
   },
   methods:{
     fetchHotsprings() {
-      this.$axios.get("hotsprings?status=open")
-        .then(res => this.hotsprings = res.data)
+      this.$axios.get('hotsprings?status=open')
+        .then(res => {
+          this.hotsprings = res.data
+          this.generateMarkers()
+        })
         .catch(err => console.log(err.status))
     },
+    CreateMap(){
+      var map = L.map("map");
+
+      L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+        attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+      }).addTo(map);
+
+      map.setView([35.3622222, 138.7313889], 5);
+
+      return map
+    },
+    generateMarkers(){
+      this.hotsprings.forEach(element => {
+        console.log(element)
+        var marker = L.marker([element.latitude, element.longtitude]).addTo(this.map)
+        marker.bindPopup(
+          "<b>"+element.name+"</b><br>"
+          +element.description+"<br>"
+          +"<a href='/'>aaa</a>"
+        )
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+#map{
+  margin-padding: 0;
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+}
+</style>
