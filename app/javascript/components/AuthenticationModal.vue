f63fc6f1c768cf388d350ca786b8b053346d6f9c
tusko-ku <tusko.ku@gmail.com>
2022-02-28 15:58:06 +0900

    Add:atuhentication form

--- app/javascript/components/AuthenticationModal.vue
+++ app/javascript/components/AuthenticationModal.vue
@@ -1,38 +1,52 @@
 <template>
 <transition name="fade">
-   <div v-if="modal_appearance">
-     <div class="modal" v-on:click.self="click">
-       <div class="modal-dialog">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h4 class="modal-title">タイトルタイトル</h4>
-             <button type="button" class="close" v-on:click="click">×</button>
-           </div>
-           <div class="modal-body">
-             <p>ダイアログの中身</p>
-             <p>ダイアログの中身</p>
-             <p>ダイアログの中身</p>
-           </div>
-           <div class="modal-footer">
-             <button type="button" class="btn btn-primary">フッターのボタンなど</button>
-           </div>
-         </div>
-       </div>
-     </div>
-     <div class="modal-backdrop show"></div>
-   </div>
- </transition>
+  <div v-if="modal_appearance">
+    <div class="modal" @click.self="close_modal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">ログイン/新規登録</h4>
+            <button type="button" class="btn-close" @click="close_modal"></button>
+          </div>
+          <div class="modal-body">
+
+            <p><input v-model="name" id="name" class=”form-control” placeholder="名前を入力してください"></p>
+            <input type="password" v-model="password" id="password" class=”form-control” placeholder="パスワードを入力してください">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" @click="submit">送信</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</transition>
 </template>
 
 <script>
 export default{
   props:['modal_appearance'],
+  data(){
+    return{
+      name: '',
+      password: ''
+    }
+  },
   mounted(){
     console.log("mount")
   },
   methods:{
-    click(){
+    close_modal(){
       this.$parent.modal_appearance=false
+    },
+    submit(){
+      this.$axios.post('authentication?name='+this.name+'&password='+this.password)
+        .then(res => {
+          this.user = res.data
+          console.log(this.user)
+        })
+        .catch(err => console.log(err.status))
     }
   },
   watch:{
