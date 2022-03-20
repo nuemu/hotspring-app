32b5304173c84344041752c5222cbd5471f2dfaf
tusko-ku <tusko.ku@gmail.com>
2022-03-20 11:37:05 +0900

    Change: heroku url 変更につきapiリクエスト変更 webpackerをいじること

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:3000/api/'
-  baseURL: 'https://polar-scrubland-90297.herokuapp.com/api/'
+  baseURL: 'https://noyu-hub.herokuapp.com//api/'
 })
 
 export default axiosInstance
