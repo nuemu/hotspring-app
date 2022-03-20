a2afa8863055fd88c8d8b0b2cbe35d8603f88751
tusko-ku <tusko.ku@gmail.com>
2022-03-20 11:32:47 +0900

    Change:deployように webpackerをいじること

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
-  //baseURL: 'https://polar-scrubland-90297.herokuapp.com/api/'
+  //baseURL: 'http://localhost:3000/api/'
+  baseURL: 'https://polar-scrubland-90297.herokuapp.com/api/'
 })
 
 export default axiosInstance
