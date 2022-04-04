2ada26f0c68333df9f73d6b1e54419e2141f95dc
tusko-ku <tusko.ku@gmail.com>
2022-04-04 18:57:01 +0900

    heroku push用

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
-  //baseURL: 'https://noyu-hub.herokuapp.com//api/'
+  //baseURL: 'http://localhost:3000/api/'
+  baseURL: 'https://noyu-hub.herokuapp.com//api/'
 })
 
 export default axiosInstance
