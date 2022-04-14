0f13050a1ba05a7f4454834c43bb82e53537b69d
tusko-ku <tusko.ku@gmail.com>
2022-04-14 16:19:39 +0900

    Fix:CORS policy again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://yutellite.com'
+    origins 'https://www.yutellite.com/'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
