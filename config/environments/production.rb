7fafcdab4f22ab0761f4bcd0900a9638b3bde0a5
tusko-ku <tusko.ku@gmail.com>
2022-04-14 14:00:02 +0900

    Add:default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options[:host] = Rails.application.routes.url_helpers.root_url(protocol: 'https')
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
