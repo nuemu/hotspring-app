59db2c8510dcc7a9e5589a54e2432234383af94a
tusko-ku <tusko.ku@gmail.com>
2022-04-14 17:46:25 +0900

    Change: CORS Policy again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://www.yutellite.com'
-    resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put], expose: ['X-CSRF-Token']
+    resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put], expose: ['X-CSRF-Token'], credentials: true
   end
 end
