7ab715c16eb377d1edc9b257813f6dd69adafb29
tusko-ku <tusko.ku@gmail.com>
2022-04-14 18:36:33 +0900

    Change: baseURLをenvからjavascript生成に

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL
+  baseURL: window.location.origin+'/api/'
 })
 
 export default axiosInstance
