cd8f114f219a4be14cd7ec0f2705b911e80048b4
nuemu <tusko.ku@gmail.com>
2022-06-24 17:06:33 +0900

    Fix: URLの文字列が存在しない時、ActiveStorageの画像が利用できるように

--- app/serializers/hotspring_serializer.rb
+++ app/serializers/hotspring_serializer.rb
@@ -3,7 +3,7 @@ class HotspringSerializer
 
   attributes :name, :latitude, :longtitude, :description, :id, :status, :prefecture
   attribute :image_url do |object|
-    if object.image_url
+    if object.image_url != ''
       object.image_url
     else
       object.generate_image_url
