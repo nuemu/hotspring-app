712996e10df039b7c8a74229dce8e0a30e58cd83
nuemu <tusko.ku@gmail.com>
2022-07-05 22:12:09 +0900

    Change: RSpecをDocker(&M1)対応

--- spec/support/driver.rb
+++ spec/support/driver.rb
@@ -1,6 +1,24 @@
+require 'capybara/rspec'
+
+Capybara.register_driver :remote_chrome do |app|
+  url = ENV['SELENIUM_DRIVER_URL']
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--no-sandbox')
+  options.add_argument('--headless')
+
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, options: options)
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    #driven_by(:selenium_chrome)
-    driven_by(:selenium_chrome_headless)
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.server_port = 4444
+    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 end
--- spec/system/hotsprings_spec.rb
+++ spec/system/hotsprings_spec.rb
@@ -1,17 +1,9 @@
-RSpec.describe 'Authentication', type: :system do
+RSpec.describe 'Hotspring', type: :system, js: true do
   describe 'hotsprings on map' do
-    context 'register' do
-      fit 'map click' do
+    context 'index page' do
+      fit 'index page' do
         visit root_path
-        click_link '始める♨︎', match: :first
-        click_button '同意'
-        page.find('li', text: '野湯登録').click()
-        page.find('#map').click([], { x: 300, y: 300 })
-        fill_in 'description', with: 'description'
-        click_button '+'
-        page.find('li', text: '-').click()
-        page.find('#map').click([], { x: 300, y: 300 })
-        expect(page).to have_content '未探索'
+        expect(page).to have_content 'ゆtellite'
       end
     end
   end
